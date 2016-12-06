46c2512e17224c1cccef9887cb44ad234654e3c6
git-afsantos <contact.andre.santos@gmail.com>
2016-12-06 18:54:49 +0000

    updated Maintainability index plugin

--- mi_calculator/plugin.py
+++ mi_calculator/plugin.py
@@ -1,7 +1,7 @@
 
 import math
 
-MI_PARAMETERS = ("lloc", "sloc", "cyclomatic_complexity", "comment_ratio",
+MI_PARAMETERS = ("lloc", "sloc", "ploc", "cyclomatic_complexity", "comment_ratio",
                  "comments", "halstead_volume")
 
 class Maintainability(object):
@@ -9,20 +9,18 @@ class Maintainability(object):
         self.mi = None
         self.lloc = None
         self.sloc = None
+        self.ploc = None
         self.ratio = None
         self.comments = None
         self.cc = 0
         self.hvol = None
 
     def compute(self):
-        if self.lloc is None:
-            if self.sloc is None:
-                return None
-            self.lloc = self.sloc
-        if self.ratio is None:
-            if self.comments is None:
-                return None
-            self.ratio = self.comments / float(self.sloc) if self.sloc else 0
+        self._prepare()
+        if not self._has_lloc():
+            return None
+        if not self._has_ratio():
+            return None
         if self.cc == 0:
             return None
         if self.hvol is None:
@@ -33,6 +31,39 @@ class Maintainability(object):
         nn_mi = (171 - 5.2 * volume_scale - .23 * self.cc - 16.2 \
                  * sloc_scale + 50 * math.sin(comments_scale))
         return min(max(0., nn_mi * 100 / 171.), 100.)
+
+    def _prepare(self):
+        if self.sloc is None:
+            if not self.comments is None:
+                if not self.ploc is None:
+                    self.sloc = self.comments + self.ploc
+                elif not self.lloc is None:
+                    self.sloc = self.comments + self.lloc
+
+    def _has_lloc(self):
+        if not self.lloc is None:
+            return True
+        if not self.ploc is None:
+            self.lloc = self.ploc
+            return True
+        if not self.sloc is None:
+            if not self.comments is None:
+                self.lloc = self.sloc - self.comments
+                return True
+            if not self.ratio is None:
+                self.lloc = self.sloc - int(self.sloc * self.ratio)
+                return True
+        return False
+
+    def _has_ratio(self):
+        if not self.ratio is None:
+            return True
+        if self.comments is None:
+            return False
+        if self.sloc is None:
+            return False
+        self.ratio = self.comments / float(self.sloc) if self.sloc else 0
+        return True
 
 def pre_process():
     return {}
@@ -47,6 +78,8 @@ def process_file_metric(iface, datum):
     if metric == "lloc":
         mi.lloc = datum.value
     elif metric == "sloc":
+        mi.sloc = datum.value
+    elif metric == "ploc":
         mi.sloc = datum.value
     elif metric == "comments":
         mi.comments = datum.value
