28d7ae8207fd284050cbaff1bda36cf7da0f2ef7
git-afsantos <contact.andre.santos@gmail.com>
2017-01-27 16:36:21 +0000

    Added license and copyright

--- ccd_plugin/plugin.py
+++ ccd_plugin/plugin.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-#Copyright (c) 2013 Dominik Borowiec, Andre Santos
+#Copyright (c) 2013 Dominik Borowiec, 2016 Andre Santos
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
