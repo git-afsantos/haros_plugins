9ac6f8774cfce27fcf6186ab6e543deb72869331
git-afsantos <contact.andre.santos@gmail.com>
2017-01-28 21:57:10 +0000

    bug fix

--- lizard_plugin/plugin.py
+++ lizard_plugin/plugin.py
@@ -24,17 +24,17 @@
 import lizard
 
 def file_analysis(iface, scope):
-	metrics = lizard.analyze_file(scope.get_path())
-	iface.report_metric("ploc", metrics.nloc)
+    metrics = lizard.analyze_file(scope.get_path())
+    iface.report_metric("ploc", metrics.nloc)
     if metrics.nloc > 400:
         iface.report_violation("max_file_length_400",
                                "File length (PLOC) of " + str(metrics.nloc))
-	for fun in metrics.function_list:
+    for fun in metrics.function_list:
         cc = fun.cyclomatic_complexity
         params = fun.parameter_count
         ploc = fun.nloc
-		iface.report_metric("cyclomatic_complexity", cc,
-							line = fun.start_line, function = fun.name)
+        iface.report_metric("cyclomatic_complexity", cc,
+                            line = fun.start_line, function = fun.name)
         if cc > 10:
             iface.report_violation("max_cyclomatic_complexity_10",
                                    "function with cyclomatic complexity " + str(cc),
@@ -45,14 +45,14 @@ def file_analysis(iface, scope):
                                    line = fun.start_line, function = fun.name)
 
         iface.report_metric("function_parameters", params,
-							line = fun.start_line, function = fun.name)
+                            line = fun.start_line, function = fun.name)
         if params > 6:
             iface.report_violation("max_function_parameters_6",
                                    "function with " + str(params) + " parameters",
                                    line = fun.start_line, function = fun.name)
 
         iface.report_metric("ploc", ploc,
-							line = fun.start_line, function = fun.name)
+                            line = fun.start_line, function = fun.name)
         if ploc > 40:
             iface.report_violation("max_function_length_40",
                                    "Function length of " + str(ploc),
