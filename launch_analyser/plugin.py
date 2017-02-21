540f7b679d3b07dc5a4f097de004d4a49345e600
git-afsantos <contact.andre.santos@gmail.com>
2017-02-21 14:07:25 +0000

    Added stub for launch analyser plugin.

---
+++ launch_analyser/plugin.py
@@ -0,0 +1,8 @@
+import os
+
+def package_analysis(iface, package):
+    count = 0
+    for sf in package.source_files:
+        if sf.language == "launch" and sf.path == os.path.join("launch", sf.name):
+            count += 1
+    print package.name, count
